245164c8c22ed8265e15e7bb1aee04a45f9f1ffc
Arturo Sánchez <danielarturosanchezgodoy@gmail.com>
2024-09-24 19:46:20 +0100

    Created project 2

---
+++ Ejercicio_MVC/Ejercicio_MVC2/Controllers/SaludosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio_MVC2.Controllers
+{
+    public class SaludosController : Controller
+    {
+        public string Index()
+        {
+            return "Hola mundo";
+        }
+
+        public string Despedida()
+        {
+            return "Adiós mundo";
+        }
+    }
+}
---
+++ Ejercicio_MVC/Ejercicio_MVC2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ejercicio_MVC2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
