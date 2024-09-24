8b450a763517f3665bc5c467821493231ad2b0f7
Arturo Sánchez <danielarturosanchezgodoy@gmail.com>
2024-09-24 20:25:24 +0100

    Ejercicio Completo

--- Ejercicio_MVC/Ejercicio_MVC2/Controllers/SaludosController.cs
+++ Ejercicio_MVC/Ejercicio_MVC2/Controllers/SaludosController.cs
@@ -1,17 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace Ejercicio_MVC2.Controllers
 {
     public class SaludosController : Controller
     {
-        public string Index()
+        public string Index(string? localidad)
         {
-            return "Hola mundo";
+            return $"Hola a {localidad}";
         }
 
-        public string Despedida()
+        public string Despedida(int ID, string nombre, string edad)
         {
-            return "Adiós mundo";
+            return $"El usuario con ID: {ID} y nombre {nombre} se despide a sus {edad} años";
         }
     }
 }
--- Ejercicio_MVC/Ejercicio_MVC2/Program.cs
+++ Ejercicio_MVC/Ejercicio_MVC2/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Saludos}/{action=Index}/{localidad?}");
 
 app.Run();
