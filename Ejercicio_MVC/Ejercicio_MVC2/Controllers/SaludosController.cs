81752ba69bae59a8de9332fddd159c80c576ae17
Arturo Sánchez <danielarturosanchezgodoy@gmail.com>
2024-09-25 19:01:43 +0100

    Fixed error on Saludos Controller

--- Ejercicio_MVC/Ejercicio_MVC2/Controllers/SaludosController.cs
+++ Ejercicio_MVC/Ejercicio_MVC2/Controllers/SaludosController.cs
@@ -5,15 +5,10 @@ namespace Ejercicio_MVC2.Controllers
 {
     public class SaludosController : Controller
     {
-<<<<<<< HEAD
+
         public IActionResult Index()
         {
             return View();
-=======
-        public string Index(string? ID)
-        {
-            return $"Hola a {ID}";
->>>>>>> 7f0110e95dd1bb95fd45766ff10fe315e2cccdc2
         }
 
         public IActionResult Despedida()
